929005722a8a2e622f59c618a24aeea9207f7a66
Sam Hart <sam.hart@angieslist.com>
2016-04-11 13:16:13 -0400

    more test, add mkdir-p

--- jpg_convert/jpg_convert.py
+++ jpg_convert/jpg_convert.py
@@ -3,11 +3,19 @@
 from __future__ import print_function
 import os
 import sys
+import errno
 
 if len(sys.argv) != 3:
     print("Error! Requires two arguments!")
     print("jpg_convert.py source/dir dest/dir")
     sys.exit(1)
+
+def mkdir_p(path):
+    try:
+        os.makedirs(directory_name)
+    except OSError as exc:
+        if exc.errno == errno.EEXIST and os.path.isdir(path):
+            pass
 
 source_dir = os.path.abspath(sys.argv[1])
 dest_dir = os.path.abspath(sys.argv[2])
@@ -15,4 +23,4 @@ dest_dir = os.path.abspath(sys.argv[2])
 for dir_name, sub_dirs, files in os.walk(source_dir):
     for fname in files:
         fn = os.path.relpath("{0}/{1}".format(dir_name, fname), source_dir)
-        print(fn)
+        print(os.path.dir_name(fn))
