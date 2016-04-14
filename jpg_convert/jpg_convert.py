b7bb9603674abeba8fe53fee64575bc4a9b49a9a
Sam Hart <criswellious@gmail.com>
2016-04-14 10:29:57 -0400

    use outfile

--- jpg_convert/jpg_convert.py
+++ jpg_convert/jpg_convert.py
@@ -29,7 +29,7 @@ for dir_name, sub_dirs, files in os.walk(source_dir):
         target_file = os.path.join(target_dir, local_file)
         source_file = os.path.join(source_dir, fn)
         mkdir_p(target_dir)
-        command = "jpegtran -progressive {0} > {1}".format(source_file, target_file)
+        command = "jpegtran -progressive {0} -outfile {1}".format(source_file, target_file)
         try:
             out = subprocess.check_output(command.split(' '))
         except:
