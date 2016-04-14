6081de5c5aca07c99b9f6c237b88c520f3192ed8
Sam Hart <sam.hart@angieslist.com>
2016-04-14 10:26:08 -0400

    wrong vari

--- jpg_convert/jpg_convert.py
+++ jpg_convert/jpg_convert.py
@@ -13,7 +13,7 @@ if len(sys.argv) != 3:
 
 def mkdir_p(path):
     try:
-        os.makedirs(directory_name)
+        os.makedirs(path)
     except OSError as exc:
         if exc.errno == errno.EEXIST and os.path.isdir(path):
             pass
